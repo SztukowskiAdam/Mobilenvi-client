b8129951cbb63e04f65b5c6bc0771a10007e9b94
SztukowskiAdam <adam-sztukowski@wp.pl>
2019-03-06 14:41:35 +0100

    User temperature read

--- src/app/Services/postify.service.ts
+++ src/app/Services/postify.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
 import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs';
+import {TokenService} from './token.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostifyService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private token: TokenService) { }
 
   register(data) {
     return this.http.post(environment.APIEndpoint + 'register', data);
@@ -17,7 +20,11 @@ export class PostifyService {
     return this.http.post(environment.APIEndpoint + 'login', data);
   }
 
-  userWeather(x, y) {
-    return this.http.post(environment.APIEndpoint + 'user/weather', x);
+  weather(data): Observable<any> {
+    const httpHeaders = new HttpHeaders({
+      'Authorization': 'Bearer ' + this.token.get()
+    });
+
+    return this.http.post(environment.APIEndpoint + 'user/weather', data, {headers: httpHeaders, observe: 'response'});
   }
 }
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,39 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {TokenService} from '../../Services/token.service';
 import {Router} from '@angular/router';
 import {PostifyService} from '../../Services/postify.service';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class DashboardComponent implements OnInit {
+  public dataa = {};
+  public weather = {
+    id: null,
+    station_id: null,
+    temperature: 20, /*default value*/
+    pressure: null,
+    created_at: null,
+    updated_at: null
+  };
 
   constructor(
     private token: TokenService,
     private route: Router,
-    private postify: PostifyService
+    private postify: PostifyService,
+    private http: HttpClient
   ) { }
 
   ngOnInit() {
     if (!this.token.loggedIn()) {
       this.route.navigateByUrl('/login');
     }
-  }
-
-  getLocation() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(this.showPosition);
+      const self = this;
+      navigator.geolocation.getCurrentPosition(function(position) {
+        self.getWeather(position);
+      });
     } else {
       console.log('Geolocation is not supported by this browser.');
     }
   }
 
-  showPosition(position) {
-    console.log(position.coords.latitude);
-    return this.postify.userWeather(position.coords.latitude, position.coords.longitude).subscribe(
-      data => console.log(data),
+  getWeather(position) {
+    this.dataa = {
+      x: position.coords.latitude,
+      y: position.coords.longitude
+    };
+
+    return this.postify.weather(this.dataa).subscribe(
+      data => this.weather = data.body.data,
       error => console.log(error)
     );
   }
